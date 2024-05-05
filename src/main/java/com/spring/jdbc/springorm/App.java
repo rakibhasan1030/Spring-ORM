332d664dd1bd5163764d40002894ec93c4077ad1
Rakib <hasan.rakib@datasoft-bd.com>
2024-05-05 15:16:12 +0600

    test again

--- src/main/java/com/spring/jdbc/springorm/App.java
+++ src/main/java/com/spring/jdbc/springorm/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Is commit is working fine?" );
+        System.out.println( "test again!" );
     }
 }
